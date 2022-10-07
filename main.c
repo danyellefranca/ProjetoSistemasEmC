46a5179099796d32caf20a9411f7e8d1d5553bbb
DANYELLE NOGUEIRA FRANCA <u21232@academico.cotuca.unicamp.br>
2022-10-07 10:49:45 -0300

    parte do abrir arquivo

--- main.c
+++ main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc,char *argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+    printf("---RESOLUCAO DE SISTEMAS LINEARES---\n");
+
+    //declara o ponteiro que vai apontar para o arquivo a ser lido
+    //define o maximo de caracteres a ser digitado como caminho do arquivo
+    char fname[100];
+    FILE* fp;
+    memset(fname, 0, 100);
+
+    //pede pro usuário digitar o nome do arquivo
+    printf("Digite o arquivo que deseja abrir: ");
+    gets(fname);
+
+    //abre o arquivo
+    fp = fopen(fname, "r");
+
+    //declaramos variaveis que vamos precisar
+    int quantLinhas = 0;
+    char sistema[80];
+    char incognitas[50];
+    char numeros[50];
+    char resultado[50];
+    char matriz;
+
+    //se o arquivo for nulo manda mensagem de erro
+    if (fp == NULL)
+    {
+        printf("\nErro ao abrir o arquivo!\n");
+        fclose(fp);
+    }
+    else
+    {
+
+    }
+
+
+ return 0;
 }
