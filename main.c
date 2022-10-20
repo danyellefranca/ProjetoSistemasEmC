130ba4d8c0e13a5a4bca314d6e4ae57b6f3e0c56
JULIA FLAUSINO DA SILVA <u21241@academico.cotuca.unicamp.br>
2022-10-20 13:12:05 -0300

    começando a juntar tudo

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+int temEquacoesEquivalentes(int ordem, float matriz[ordem][ordem]);
+void printarMatriz(int numLinhas, int numColunas, float matriz[numLinhas][numColunas]);
+
 int main(int argc,char *argv[])
 {
     printf("---RESOLUCAO DE SISTEMAS LINEARES---\n");
@@ -9,7 +12,7 @@ int main(int argc,char *argv[])
     //declara o ponteiro que vai apontar para o arquivo a ser lido
     //define o maximo de caracteres a ser digitado como caminho do arquivo
     char fname[100];
-    FILE* arq;
+    FILE *arq;
     memset(fname, 0, 100);
 
     //pede pro usuário digitar o nome do arquivo
@@ -28,9 +31,7 @@ int main(int argc,char *argv[])
     else
     {
         char linha;
-        char incognitas[30];
         char coeficientes[300];
-        char resultado[50];
 
         // para ler a primeira linha linha
         int ordem;
@@ -40,31 +41,15 @@ int main(int argc,char *argv[])
             exit(1);
         }
 
-        // int quantasLinhas = ordem;
+        int ilinha  = ordem;     // quantas linhas tem
+        int jcoluna = ordem + 1; // quantas colunas tem
 
-        int ilinha = ordem;
-        int jcoluna = ordem + 1;
-
-        // lê o conteudo da linha e coloca no vetor sistema
-
-        char *sistema[ordem];
-
-        int indiceIndice = 0;
-
-        // eof ==> end of file
-        /*
-        while (!fp-->eof())
-        {
-            fscanf(arq,"%d",&linha);
-            sistema[indiceIndice++] = linha;
-        }
-        */
-
+        // matriz com o sistema completo (incluindo a coluna de resultados)
         float matriz[ilinha][jcoluna];
 
-        for (int x=0; x < ordem; x++)
+        for (int x = 0; x < ordem; x++)
         {
-            for(int y=0; y<ordem; y++)
+            for(int y = 0; y<ordem+1; y++)
             {
                 fscanf(arq,"%f ",&matriz[ilinha][jcoluna]);
                 printf("%f ", matriz[ilinha][jcoluna]);
@@ -72,9 +57,11 @@ int main(int argc,char *argv[])
             printf("\n");
         }
 
+
         // lê linha por linha e guardar sistemas em um vetor
         // pegar o que tá nesse vetor sistemas e divide os coeficientes
 
+        /*
         int indiceCoef = 0;
 
         for (int i = 0; i < ordem; i++)
@@ -89,11 +76,73 @@ int main(int argc,char *argv[])
               token = strtok(NULL, " ");
            }
            indiceCoef = 0;
+        }*/
 
+        float matriz_coef[ilinha][ilinha]; // matriz com os coeficientes da equação
+
+        for (int linha = 0; linha < ordem; linha++)
+        {
+            for (int coluna = 0; coluna < ordem; coluna++)
+            {
+                matriz_coef[linha][coluna] = matriz[linha][coluna];
+            }
+        }
+
+        if (temEquacoesEquivalentes(ilinha, matriz_coef))
+        {
+            printf()
         }
 
     }
     fclose(arq);
 
+
+
  return 0;
+}
+
+int temEquacoesEquivalentes(int ordem, float matriz[ordem][ordem])
+{
+    float resultadoDaDivisao[ordem];
+    float modelo;
+    int linha1, linha2, coluna, elemento;
+    int saoEquivalentes = 1; // sao equivalentes começa com true
+
+    for (linha1 = 0; linha1 < ordem; linha1++)
+    {
+        for (linha2 = linha1 + 1; linha2 < ordem; linha2++)
+        {
+            for (coluna = 0; coluna < ordem; coluna++)
+            {
+                float num = matriz[linha1][coluna] / matriz[linha2][coluna];
+                resultadoDaDivisao[coluna] = num;
+            }
+
+            for (elemento = 1; elemento < ordem; elemento++)
+            {
+                if (resultadoDaDivisao[0] != resultadoDaDivisao[elemento])
+                {
+                    saoEquivalentes = 0;
+                }
+            }
+
+            if (saoEquivalentes) return saoEquivalentes;
+        }
+
+    }
+
+    return saoEquivalentes;
+}
+
+void printarMatriz(int numLinhas, int numColunas, float matriz[numLinhas][numColunas])
+{
+    int linha, coluna;
+    for (linha = 0; linha < numLinhas; linha ++)
+    {
+        for (coluna = 0; coluna < numColunas; coluna++)
+        {
+            printf("[%.0f]", matriz[linha][coluna]);
+        }
+        printf("\n");
+    }
 }
